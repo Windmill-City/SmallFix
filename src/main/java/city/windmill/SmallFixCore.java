8131313a44de410734a9af8911e0088d3a13672a
Windmill-City <1449182174@qq.com>
2020-05-13 11:09:09 +0800

    Initial commit

---
+++ src/main/java/city/windmill/SmallFixCore.java
@@ -0,0 +1,47 @@
+package city.windmill;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+@IFMLLoadingPlugin.Name("SmallFixCore")
+@IFMLLoadingPlugin.MCVersion("1.12.2")
+public class SmallFixCore implements IFMLLoadingPlugin {
+    public static Logger LOGGER = LogManager.getLogger("SmallFixCore");
+
+    public SmallFixCore() {
+        LOGGER.info("setting up mixin environment");
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixin.json");
+    }
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
